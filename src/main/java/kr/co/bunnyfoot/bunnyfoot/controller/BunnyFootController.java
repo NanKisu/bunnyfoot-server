b7c7076e48012951fe5ec2b89b26e650b2e6381b
nankisu0301 <nankisu0301@naver.com>
2021-03-08 22:17:29 +0900

    @CrossOrigin POST 허용

--- src/main/java/kr/co/bunnyfoot/bunnyfoot/controller/BunnyFootController.java
+++ src/main/java/kr/co/bunnyfoot/bunnyfoot/controller/BunnyFootController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +28,7 @@ import kr.co.bunnyfoot.bunnyfoot.dto.PredictResDto;
 import kr.co.bunnyfoot.bunnyfoot.feign.PredictClient;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin(origins = "*", methods = RequestMethod.POST)
 @RequestMapping("/")
 public class BunnyFootController {
   
