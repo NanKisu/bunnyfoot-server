ddba1c2ff573683680ab6dd4a24497504dccf529
nankisu0301 <nankisu0301@naver.com>
2021-03-31 23:23:19 +0900

    swagger host 변경

--- src/main/java/kr/co/bunnyfoot/bunnyfoot/config/SpringFoxConfig.java
+++ src/main/java/kr/co/bunnyfoot/bunnyfoot/config/SpringFoxConfig.java
@@ -13,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SpringFoxConfig {
   @Bean
   public Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+    return new Docket(DocumentationType.SWAGGER_2).host("https://bbti.site").select().apis(RequestHandlerSelectors.any())
         .paths(PathSelectors.any()).build();
   }
 }
