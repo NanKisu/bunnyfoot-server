48dc9857f5edbdc913d5aca956707556bbe93ff4
nankisu0301 <nankisu0301@naver.com>
2021-09-01 19:06:20 +0900

    cors 수정

---
+++ src/main/java/kr/co/bunnyfoot/bunnyfoot/config/BunnyFootWebConfig.java
@@ -0,0 +1,11 @@
+package kr.co.bunnyfoot.bunnyfoot.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class BunnyFootWebConfig implements WebMvcConfigurer{
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+      registry.addMapping("/**").allowedOrigins("*");
+  }
+}
